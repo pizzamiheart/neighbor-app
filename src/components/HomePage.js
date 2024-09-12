87cf582169eeb6f509b44e5c9da3a900af224a42
Andrew Ginn <andrewginn@Andrews-Air.lan>
2024-09-12 13:44:45 -0400

    still optimizing for mobile on home page. moving message and call buttons above the scroll

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -16,24 +16,6 @@ function HomePage() {
       </header>
       <main>
         <h1>Welcome to Neighbor!</h1>
-        <div className="statements-container">
-          <div className="statement">
-            <p>What if you could call one person who is always available and knows everything there is to know about your technology?</p>
-          </div>
-          <div className="statement">
-            <p>Well, that person doesn't exist but you can call your personal tech assistant Neighbor!</p> 
-            <p>Neighbor is an AI who's always around and ready to help!</p>
-          </div>
-        </div>
-        <div className="info-container">
-          <div className="info">
-            <p>Printer won't print? Can't connect to your WiFi? Locked out of an account?</p> 
-            <p>It's hard to know who to call to fix it quickly!</p>
-          </div>
-          <div className="info">
-            <p>Neighbor eliminates that problem by being a message or a call away!</p>
-          </div>
-        </div>
         <div className="button-container">
           <Link to="/message" className="button">
             Message Neighbor
@@ -41,6 +23,22 @@ function HomePage() {
           <Link to="/call" className="button">
             Call Neighbor
           </Link>
+        </div>
+        <div className="statements-container">
+          <div className="statement">
+            <p>What if you could call one person who is always available and knows everything there is to know about your technology?</p>
+          </div>
+          <div className="statement">
+            <p>Well, that person doesn't exist but you can call your personal tech assistant Neighbor! An AI who's always around and ready to help!</p>
+          </div>
+        </div>
+        <div className="info-container">
+          <div className="info">
+            <p>Printer won't print? Can't connect to your WiFi? Locked out of an account? It's hard to know who to call to fix it quickly!</p>
+          </div>
+          <div className="info">
+            <p>Neighbor eliminates that problem by being a message or a call away!</p>
+          </div>
         </div>
       </main>
     </div>
