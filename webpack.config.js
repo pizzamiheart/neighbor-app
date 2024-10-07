8786761b1d16db611728f35b519555fbfb36a1c7
Andrew Ginn <andrewginn@Andrews-Air.attlocal.net>
2024-10-06 21:41:44 -0500

    added auth, considering making app more gated, decided against, upgraded model to gpt 4o-mini

--- webpack.config.js
+++ webpack.config.js
@@ -39,10 +39,7 @@ module.exports = {
       filename: 'index.html',
       inject: 'body' // This ensures the script is injected into the <body> tag
     }),
-    new webpack.DefinePlugin(envKeys),
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(process.env)
-    })
+    new webpack.EnvironmentPlugin(['REACT_APP_FIREBASE_API_KEY', 'REACT_APP_FIREBASE_AUTH_DOMAIN', 'REACT_APP_FIREBASE_PROJECT_ID', 'REACT_APP_FIREBASE_STORAGE_BUCKET', 'REACT_APP_FIREBASE_MESSAGING_SENDER_ID', 'REACT_APP_FIREBASE_APP_ID', 'REACT_APP_FIREBASE_MEASUREMENT_ID'])
   ],
   devServer: {
     historyApiFallback: true,
